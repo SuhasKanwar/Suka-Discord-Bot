7aba93d2c8457728e10d35982147d0cce342d949
SuhasKanwar <suhas.kanwar@gmail.com>
2024-12-18 07:35:49 +0530

    Fixing Deployment

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ client.on('interactionCreate', handleInteraction);
 client.login(process.env.DISCORD_BOT_TOKEN);
 
 app.get('/', (req, res) => {
-    return res.end("Bot is live and working fine\nUse this link to add it to your discord -> https://discord.com/oauth2/authorize?client_id=1284383939896803349&permissions=8&integration_type=0&scope=bot")
+    return res.write("Bot is live and working fine\nUse this link to add it to your discord -> https://discord.com/oauth2/authorize?client_id=1284383939896803349&permissions=8&integration_type=0&scope=bot")
 })
 
 app.listen(PORT, (err) => {
